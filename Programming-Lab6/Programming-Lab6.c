790e10bc1ab4f608cdf05b3c991418ed70df0a5d
Kurin, Alex <olexkurin@gmail.com>
2021-10-21 21:34:56 +0300

    lab6

---
+++ Programming-Lab6/Programming-Lab6.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("Enter n:\n");
+	scanf_s("%d", &n);
+
+	double x = 1.0;
+	double y = 1.0;
+	double sum = 0.0;
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += x / (1.0 + y);
+		y += x;
+		x *= 0.3;
+	}
+
+	printf("S = %lf", sum);
+
+	getch();
+	return 0;
+}
